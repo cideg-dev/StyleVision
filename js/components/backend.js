0a87e71d711897546d139fe1e37d811fa00a85f9
cideg-dev <cideg.p.line@gmail.com>
2025-10-29 07:46:04 +0100

    Fix: Corrected syntax error in backend.js

--- js/components/backend.js
+++ js/components/backend.js
@@ -1,24 +1,56 @@
-// Dans BackendSimulator class
-addVehicle(vehicle) {
-    const data = this.getData();
-    if (!data.vehicles) data.vehicles = [];
-    data.vehicles.push(vehicle);
-    this.saveData(data);
-    return vehicle;
-}
+class BackendSimulator {
+    constructor() {
+        this.data = this.loadInitialData();
+    }
 
-addMannequin(mannequin) {
-    const data = this.getData();
-    if (!data.mannequins) data.mannequins = [];
-    data.mannequins.push(mannequin);
-    this.saveData(data);
-    return mannequin;
-}
+    loadInitialData() {
+        const storedData = localStorage.getItem('stylevision_data');
+        if (storedData) {
+            return JSON.parse(storedData);
+        }
+        return {
+            clothingItems: {
+                top: [],
+                bottom: [],
+                shoes: [],
+                accessory: []
+            },
+            hairstyles: [],
+            vehicles: [],
+            mannequins: []
+        };
+    }
 
-getSettings() {
-    return JSON.parse(localStorage.getItem('stylevision_settings')) || {};
-}
+    getData() {
+        return this.data;
+    }
 
-saveSettings(settings) {
-    localStorage.setItem('stylevision_settings', JSON.stringify(settings));
+    saveData(data) {
+        this.data = data;
+        localStorage.setItem('stylevision_data', JSON.stringify(this.data));
+    }
+
+    addVehicle(vehicle) {
+        const data = this.getData();
+        if (!data.vehicles) data.vehicles = [];
+        data.vehicles.push(vehicle);
+        this.saveData(data);
+        return vehicle;
+    }
+
+    addMannequin(mannequin) {
+        const data = this.getData();
+        if (!data.mannequins) data.mannequins = [];
+        data.mannequins.push(mannequin);
+        this.saveData(data);
+        return mannequin;
+    }
+
+    getSettings() {
+        return JSON.parse(localStorage.getItem('stylevision_settings')) || {};
+    }
+
+    saveSettings(settings) {
+        localStorage.setItem('stylevision_settings', JSON.stringify(settings));
+    }
 }
